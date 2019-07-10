0f2ccfe51aa0d6af2ee986f5e7ec38a28c1a2fca
liyang_a <liyangly@liyanglydeMacBook-Pro.local>
2019-07-10 10:56:10 +0800

    1.0.5

---
+++ YNCommonUtils/Category/NSString+YNOrder.h
@@ -0,0 +1,19 @@
+//
+//  NSString+YNOrder.h
+//  YNCommonUtilsDemo
+//
+//  Created by liyangly on 2019/7/10.
+//  Copyright © 2019 liyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (YNOrder)
+
+- (NSString *)getRandomLetterStringWithCount:(NSInteger)count;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YNCommonUtils/Category/NSString+YNOrder.m
@@ -0,0 +1,26 @@
+//
+//  NSString+YNOrder.m
+//  YNCommonUtilsDemo
+//
+//  Created by liyangly on 2019/7/10.
+//  Copyright © 2019 liyang. All rights reserved.
+//
+
+#import "NSString+YNOrder.h"
+
+@implementation NSString (YNOrder)
+
+- (NSString *)getRandomLetterStringWithCount:(NSInteger)count {
+    
+    NSArray *allChars = @[@"a", @"b", @"c", @"d", @"e", @"f", @"g", @"h", @"i", @"j", @"k", @"l", @"m", @"n", @"o", @"p", @"q", @"r", @"s", @"t", @"u", @"v", @"w", @"x", @"y", @"z"];
+    
+    NSMutableString *tmpStr = [NSMutableString new];
+    for (int i=0; i<count; i++) {
+        
+        [tmpStr appendString:allChars[arc4random()%allChars.count]];
+    }
+    
+    return tmpStr;
+}
+
+@end
